87e0206d1aa6ded15c2037558a55da804a0ecdc1
afaf-tech <fatihalfikri5@gmail.com>
2020-04-22 15:17:28 +0700

    #36 mongoose init

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,29 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+// Connect to the database
+// mongodb+srv://test1:<password>@cluster0-qzmeg.mongodb.net/test?retryWrites=true&w=majority
+// mongoose.connect('mongodb:')
+const uri = 'mongodb+srv://ninja:ninja@cluster0-qzmeg.mongodb.net/test?retryWrites=true&w=majority';
+// Prints "MongoError: bad auth Authentication failed."
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=>{
+    console.log("mongodb is connected");
+}).catch(err => {console.log('err',err) ;console.log('hahahaha')});
+
+//create schema like blueprint
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var item1 = Todo({item: 'buy flowes'}).save(function(err){
+    if(err) throw err;
+    console.log('item saved');
+})
+
+
 // repo
 var data = [{ item: 'get milk'},{item:'walk dog'},{item:'kick some coding area.'}]
 
