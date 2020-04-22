0549f04849ab824ddd650e7db3e0a0b459c2ca91
afaf-tech <fatihalfikri5@gmail.com>
2020-04-22 15:31:02 +0700

    #37

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -18,34 +18,45 @@ var todoSchema = new mongoose.Schema({
 });
 
 var Todo = mongoose.model('Todo', todoSchema);
-var item1 = Todo({item: 'buy flowes'}).save(function(err){
-    if(err) throw err;
-    console.log('item saved');
-})
+
+// var item1 = Todo({item: 'buy flowes'}).save(function(err){
+//     if(err) throw err;
+//     console.log('item saved');
+// })
 
 
 // repo
-var data = [{ item: 'get milk'},{item:'walk dog'},{item:'kick some coding area.'}]
+// var data = [{ item: 'get milk'},{item:'walk dog'},{item:'kick some coding area.'}]
 
 var urlencodedParser = bodyParser.urlencoded({extended:true});
 
 
 module.exports = function(app){
     app.get('/todo', function(req,res){
-        // console.log(data);
-        res.render('todo', {todos:data});
+        // get data from mongodb and pass it to the view      
+        Todo.find({}, function(err,data){
+            if( err) throw err;
+            // pass data to view
+            res.render('todo', {todos:data});
+        });  
     });
 
     app.post('/todo', urlencodedParser, function(req,res){
-        data.push(req.body);
+        // get data from the view and add it to mongodb
+        var newTodo = Todo(req.body).save(function(err,data){
+            if(err) throw err;
+            res.json(data);
+        }) // req.body = {item: 'item'}
+        // data.push(req.body);
         // send data back to the client 
-        console.log(req.body)
-        res.json(data);
     });
 
-
     app.delete('/todo/:item', function(req,res){
-        data = data.filter(todo=> todo.item.replace(/ /g, '-')!==req.params.item);
-        res.json(data);
+        // delete the requested items fom mongodb
+        Todo.find({item:req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+            if(err)throw err;
+            res.json(data);
+        })
+        // data = data.filter(todo=> todo.item.replace(/ /g, '-')!==req.params.item);
     });
 }
